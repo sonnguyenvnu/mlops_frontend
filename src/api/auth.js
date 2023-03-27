130598426813e2d471f9c533bdc66c1c2d70dbd2
nvquynh29 <nvquynh29@gmail.com>
2023-03-27 16:52:39 +0700

    Fix CORS issue

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,12 @@
 import instance from './axios'
+import axios from 'axios'
 
-const signup = (credential) => instance.post('/auth/signup', credential)
-const login = (credential) => instance.post('/auth/login', credential)
+const instanceWithoutCredential = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+})
+
+const signup = (credential) => instanceWithoutCredential.post('/auth/signup', credential)
+const login = (credential) => instanceWithoutCredential.post('/auth/login', credential)
 const refreshToken = () => instance.get('/auth/refresh-token')
 
 export { signup, login, refreshToken }
